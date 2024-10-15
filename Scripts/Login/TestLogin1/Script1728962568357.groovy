54133c4949e74ac50c30714d7b8e5c5fc88126cf
Aliffrianto Yudha Prakoso <prakosoyudha56@gmail.com>
2024-10-15 10:29:13 +0700

    Add Github

---
+++ Scripts/Login/TestLogin1/Script1728962568357.groovy
@@ -0,0 +1,36 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+// open browser
+WebUI.openBrowser("https://www.saucedemo.com/")
+
+// input username
+WebUI.setText(findTestObject('Object Repository/Test1_OR/Page_Swag Labs/input_Swag Labs_user-name'), "standard_user")
+
+// input password
+WebUI.setText(findTestObject('Object Repository/Test1_OR/Page_Swag Labs/input_Swag Labs_password'), "secret_sauce")
+
+// click login button
+WebUI.click(findTestObject('Object Repository/Test1_OR/Page_Swag Labs/input_Swag Labs_login-button'))
+
+// verify page
+WebUI.verifyElementPresent(findTestObject('Object Repository/Test1_OR/Page_Swag Labs/span_Products'), 4)
+
+// close browser
+WebUI.closeBrowser()
